b933968dcb0aedd1e3992745322a3b2bc478fd03
tokkaya79 <tokkaya79@gmail.com>
2023-06-11 11:07:29 +0300

    created fould sections

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { HeaderComponent } from './components/header/header.component';
+import { HeaderComponent } from './sections/header/header.component';
 import { NavBarComponent } from './components/navBar/navBar.component';
 import { LogoComponent } from './components/logo/logo.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { ButtonComponent } from './components/button/button.component';
-import { LogosSectionComponent } from './components/logos-section/logos-section.component';
-import { FindSoundSectionComponent } from './components/find-sound-section/find-sound-section.component';
+import { LogosSectionComponent } from './sections/logos-section/logos-section.component';
+import { FindSoundSectionComponent } from './sections/find-sound-section/find-sound-section.component';
 import { TitleComponent } from './shared/title/title.component';
 
 
